3b657f0070c35133ccbe64e5680e4b64f0e5616e
ymtnez <yohandysm@gmail.com>
2023-03-28 09:12:36 -0400

    Update index.js
    Quitando el cambio que hizo VSC

--- src/index.js
+++ src/index.js
@@ -11,6 +11,3 @@ const divRoot = document.querySelector('#root');
 //const divRoot = document.getElementById('root');
 
 ReactDOM.render(<CounterApp value = { 10 }/>, divRoot);
-
-
-//desde ymtnez-upr
