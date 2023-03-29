b54d608d5e8c0eb216132a0942f5e195ab4a9412
Yohandys <ymtnez@upr.edu.cu>
2023-03-29 14:50:06 -0500

    Agregando la prueba 08-, 09- y 11-

--- src/tests/base/08-imp-exp.test.js
+++ src/tests/base/08-imp-exp.test.js
@@ -1,10 +1,37 @@
-import { getHeroeById } from '../../base/08-imp-exp';
+import { getHeroeById, getHeroesByOwner } from '../../base/08-imp-exp';
+import heroes from '../../data/heroes';
 
 describe('Pruebas en funciones de Heroes', () => {
     test('Debe retornar un heroe por id', () => {
-        const id = 2;
+        const id = 1;
         const heroe = getHeroeById(id);
-
-        //expect(espera).toBe(recibe);
+        const heroeData = heroes.find( (objeto) => (objeto.id === id) );
+        
+        expect(heroe).toEqual(heroeData);
+        
     });
+
+    test('Debe retornar undefined si heroe no existe', () => {
+        const id = 10;
+        const heroe = getHeroeById(id); const heroeData = heroes.find ( (elem) => (elem.id === id) );
+        
+        expect(heroe).toBeUndefined; //expect(heroe).toBe(undefined);
+    })
+
+    test('Debe retornar un arreglo con los heroes de DC', () => {
+        const owner = 'DC';
+        const dc = getHeroesByOwner (owner);
+        const dcData = heroes.filter( (param) => (param.owner === owner) );
+
+        expect( dc ).toEqual( dcData );
+    })
+    
+    test('Debe retornar un arreglo con los heroes de Marvel', () => {
+        const owner = 'Marvel';
+        const marvel = getHeroesByOwner(owner);
+        const marvelData = heroes.filter( (objetos) => (objetos.owner === owner) );
+
+        expect( marvel ).toEqual( marvelData );
+        expect( marvel.length ).toBe( 2 );
+    })
 });
---
+++ src/tests/base/09-promesas.test.js
@@ -0,0 +1,23 @@
+import { getHeroeByIdAsync } from '../../base/09-promesas';
+import heroes from '../../data/heroes';
+
+describe('Pruebas con promesas', () => {
+    test('getHeroeByIdAsync debe retornar un Heroe async', ( done ) => {
+        const id = 1;
+        getHeroeByIdAsync(id)
+            .then( heroe => {
+                expect(heroe).toBe(heroes[0]);
+                done();
+            });
+    });
+
+    test('debe obtener un error si el heroe por id no existe', ( done ) => {
+        const id = 10;
+        getHeroeByIdAsync(id)
+            .catch( err => {
+                expect( err ).toBe('No se pudo encontrar el héroe');
+                done();
+            });
+        
+    });
+}); 
